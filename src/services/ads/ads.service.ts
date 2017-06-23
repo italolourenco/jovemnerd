8c9c4e11933e8852b243a3ab3a4c86540bb5c7fc
Italo Lourenço <italolt10@gmail.com>
2017-06-22 23:31:07 -0300

    final

--- src/services/ads/ads.service.ts
+++ src/services/ads/ads.service.ts
@@ -38,7 +38,7 @@ init(){
             bgColor: 'black', // color name, or '#RRGGBB'
             // x: integer,     // valid when set position to 0 / POS_XY
             // y: integer,     // valid when set position to 0 / POS_XY
-            isTesting: true, // set to true, to receiving test ad for testing purpose
+            isTesting: false, // set to true, to receiving test ad for testing purpose
             // autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
     };
     this.admob.setOptions(this._opt);
